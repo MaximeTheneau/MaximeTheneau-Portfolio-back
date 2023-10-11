4c4725a779a0080d870b35bf52f9579254919697
Theneau Maxime <theneau.maxime@gmail.com>
2023-10-11 15:10:36 +0200

    test webhook deploy

--- src/Controller/Api/WebhookController.php
+++ src/Controller/Api/WebhookController.php
@@ -26,26 +26,16 @@ class WebhookController extends ApiController
         $body = $request->getContent();
         $calculatedSignature = 'sha256=' . hash_hmac('sha256', $body, $authToken);
 
-        // if ($signature !== $calculatedSignature) {
-        //     return new JsonResponse('Unauthorized request!', 401);
-        // }
-        // $stashProcess = new Process(['git', 'stash']);
-        // $stashProcess->run();
-
-        // if (!$stashProcess->isSuccessful()) {
-        //     return new JsonResponse('Git stash failed', 500);
-        // }
-
 
         $pullProcess = new Process(['git', 'pull', 'origin', 'main']);
         $pullProcess->run();
+        if ($pullProcess->isSuccessful()) {
             return new JsonResponse('Git pull successful', 200);
+        } else {
+            return new JsonResponse('Git pull failed: ' . $pullProcess->getErrorOutput(), 500);
+        }
+        
 
-        // if ($pullProcess->isSuccessful()) {
-        //     return new JsonResponse('Git pull successful', 200);
-        // } else {
-        //     return new JsonResponse('Git pull failed', 500);
-        // }
 
     }
 }
