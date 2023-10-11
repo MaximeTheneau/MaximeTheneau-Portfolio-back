f8e75213ae55bd8d21092b5356f3b3c301ce6503
Theneau Maxime <theneau.maxime@gmail.com>
2023-10-11 14:09:48 +0200

    Feat: Controller for webhook github

---
+++ src/Controller/Api/WebhookController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Process\Process;
+
+/**
+ * @Route("/api")
+ */
+class WebhookController extends ApiController
+{
+    
+    /**
+     * @Route("/webhook/github", name="webhook_github", methods={" POST", "GET"})
+     */
+    public function handleWebhook(Request $request): JsonResponse
+    {
+        $authToken = $_ENV['AUTH_TOKEN_WEBHOOK']; // Assurez-vous que votre jeton d'authentification est stocké dans les variables d'environnement.
+
+        // Vérifiez la signature HMAC
+        $signature = $request->headers->get('X-Hub-Signature-256');
+        $body = $request->getContent();
+        $calculatedSignature = 'sha256=' . hash_hmac('sha256', $body, $authToken);
+
+        if ($signature !== $calculatedSignature) {
+            return new JsonResponse('Unauthorized request!', 401);
+        }
+
+        // Exécutez "git stash" pour sauvegarder les modifications locales
+        $stashProcess = new Process(['git', 'stash']);
+        $stashProcess->run();
+
+        if (!$stashProcess->isSuccessful()) {
+            return new JsonResponse('Git stash failed', 500);
+        }
+
+        // Exécutez "git pull" pour mettre à jour depuis la branche principale
+        $pullProcess = new Process(['git', 'pull', 'origin', 'main']);
+        $pullProcess->run();
+
+        if ($pullProcess->isSuccessful()) {
+            return new JsonResponse('Git pull successful', 200);
+        } else {
+            return new JsonResponse('Git pull failed', 500);
+        }
+    }
+}
