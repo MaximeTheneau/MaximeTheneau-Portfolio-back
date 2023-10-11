1f3efcb9862034b3d61a69403e44d430f3b9ec2e
Theneau Maxime <theneau.maxime@gmail.com>
2023-10-11 15:07:02 +0200

    test webhook deploy

--- src/Controller/Api/WebhookController.php
+++ src/Controller/Api/WebhookController.php
@@ -19,7 +19,7 @@ class WebhookController extends ApiController
      */
     public function handleWebhook(Request $request): JsonResponse
     {   
-        // test
+        // test rrr
         $authToken = $_ENV['AUTH_TOKEN_WEBHOOK']; 
 
         $signature = $request->headers->get('X-Hub-Signature-256');
