85ba745513597311dd0958cbaaffde6e2cb045c4
Theneau Maxime <theneau.maxime@gmail.com>
2023-10-11 15:23:52 +0200

    test webhook deploy

--- src/Controller/Api/WebhookController.php
+++ src/Controller/Api/WebhookController.php
@@ -20,34 +20,25 @@ class WebhookController extends ApiController
     public function handleWebhook(Request $request): JsonResponse
     {   
         // Mise de côté des modifications non validées
-$stashProcess = new Process(['git', 'stash']);
-$stashProcess->run();
 
-if ($stashProcess->isSuccessful()) {
+
+
     // Le stash a réussi, vous pouvez maintenant effectuer le tirage
     $authToken = $_ENV['AUTH_TOKEN_WEBHOOK']; 
     $signature = $request->headers->get('X-Hub-Signature-256');
     $body = $request->getContent();
     $calculatedSignature = 'sha256=' . hash_hmac('sha256', $body, $authToken);
-
+    $stashProcess = new Process(['git', 'stash']);
+    $stashProcess->run();
     $pullProcess = new Process(['git', 'pull', 'origin', 'main']);
     $pullProcess->run();
-    if ($pullProcess->isSuccessful()) {
-        // Tirage réussi, reprenez les modifications mises de côté
-        $popStashProcess = new Process(['git', 'stash', 'pop']);
-        $popStashProcess->run();
 
-        if ($popStashProcess->isSuccessful()) {
-            return new JsonResponse('Git pull successful', 200);
-        } else {
-            return new JsonResponse('Failed to pop the stash', 500);
-        }
+    if ($pullProcess->isSuccessful()) {
+        return new JsonResponse('Git pull successful', 200);
     } else {
         return new JsonResponse('Git pull failed: ' . $pullProcess->getErrorOutput(), 500);
     }
-} else {
-    return new JsonResponse('Git stash failed', 500);
-}
+
 
     }
 }
