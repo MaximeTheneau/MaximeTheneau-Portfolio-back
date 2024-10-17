8c8e2870dbce4fe7a119c97a06d5fc2d3322dff5
Theneau Maxime <theneau.maxime@gmail.com>
2024-10-17 16:51:27 +0200

    Feat create directory form

--- src/Controller/Api/ContactController.php
+++ src/Controller/Api/ContactController.php
@@ -82,6 +82,95 @@ class ContactController extends ApiController
                 Response::HTTP_INTERNAL_SERVER_ERROR,
             );
         }
+    }
+    #[Route('&directory', name: 'add_contact_directory', methods: ['POST'])]
+    public function addDirectory(Request $request, MailerInterface $mailer, ValidatorInterface $validator): JsonResponse
+    {
+
+    $content = $request->getContent();
+    $data = json_decode($content, true);
+
+    $constraintViolationList = $validator->validate($data['siteWeb'], [
+        new Assert\Url(),
+    ]);
+
+    $constraintViolationList = $validator->validate($data['siteWeb'], [
+        new Assert\Regex([
+            'pattern' => '/^(https:\/\/)/',
+            'message' => "Le site web doit commencer par 'https://'",
+        ]),
+    ]);
+
+
+    if (
+        empty($data['name']) ||
+        empty($data['email']) ||
+        empty($data['location']) ||
+        empty($data['postalCode']) ||
+        empty($data['siteWeb']) ||
+        empty($data['service']) ||
+        empty($data['subject']) ||
+        empty($data['directory'])
+        ) {
+        return $this->json(
+            [
+                "erreur" => "Erreur de saisie",
+                "code_error" => 404
+            ],
+            Response::HTTP_NOT_FOUND, // 404
+        );
+    }
+
+    if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+        return $this->json(
+            [
+                "erreur" => "Adresse e-mail invalide",
+                "code_error" => 400
+            ],
+            Response::HTTP_BAD_REQUEST, // 400
+        );
+    }
+    
+    if ($data['subject'] === 'Webmaster') {
+        $data['subject'] = 'Demande de contact webmaster';
+        $emailTo = $_ENV['MAILER_TO_WEBMASTER'];
+    }
+    else {
+        return $this->json(
+            [
+                "erreur" => "Erreur de saisie",
+                "code_error" => 400
+            ],
+            Response::HTTP_BAD_REQUEST, // 400
+        );
+    }
+    $email = (new TemplatedEmail())
+        ->to($emailTo)
+        ->from($_ENV['MAILER_TO'])
+        ->subject($data['subject'] . ' de ' . $data['name'])
+        ->htmlTemplate('emails/contactDirectory.html.twig')
+        ->context([
+            'emailContact' => $data['email'],
+            'subjectContact' => $data['subject'],
+            'nameContact' => $data['name'],
+            'postalCodeContact' => $data['postalCode'],
+            'locationContact' => $data['location'],
+            'websiteContact' => $data['siteWeb'],
+            'serviceContact' => $data['service'],
+            'directoryContact' => $data['directory'],
+            'directoryOtherContact' => $data['directoryOther'] ?? null,
+
+        ])
+        ->replyTo($data['email']);
+
+    $mailer->send($email);
+
+    return $this->json(
+        [
+            "message" => "Votre message a bien été envoyé",
+        ],
+        Response::HTTP_OK,
+    );
     }   
 
 }
