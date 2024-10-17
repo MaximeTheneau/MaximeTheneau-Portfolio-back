da0031a4156a2fd8eb8bd1588dc3af03ae637113
Theneau Maxime <theneau.maxime@gmail.com>
2024-10-17 16:57:06 +0200

    Feat create devis  form

--- src/Controller/Api/ContactController.php
+++ src/Controller/Api/ContactController.php
@@ -31,146 +31,150 @@ class ContactController extends ApiController
         $this->serializer = $serializer;
     }
 	
-        #[Route('', name: 'add_contact', methods: ['POST'])]
-        public function add(Request $request, MailerInterface $mailer): JsonResponse
-        {
-
-        $content = $request->getContent();
-        $data = json_decode($content, true);
-
-        try {
-            
-
-            $email = (new TemplatedEmail())
-                ->to($_ENV['MAILER_FROM'])
-                ->from($data['email'])
-                ->subject('nouveau message de ' . $data['name'])
-                ->htmlTemplate('emails/contact.html.twig')
-                ->context([
-                    'emailContact' => $data['email'],
-                    'subjectContact' => $data['subject'],
-                    'nameContact' => $data['name'],
-                    'messageContact' => $data['message'],
-                ])
-                ->replyTo($data['email']);
-    
-            $mailer->send($email);
-    
-            return $this->json(
-                [
-                    "message" => "Votre message a bien été envoyé",
-                ],
-                Response::HTTP_OK,
-            );
-
-        } catch (\Exception $e) {
-            $email = (new TemplatedEmail())
-            ->to($_ENV['MAILER_FROM'])
-            ->from($_ENV['MAILER_TO'])
-            ->subject('Erreur lors de l\'envoie de l\'email')
-            ->htmlTemplate('emails/error.html.twig')
-            ->context([
-                'error' => $e->getMessage(),
-            ]);
-            $mailer->send($email);
-            
-            
-            return $this->json(
-                [
-                    "message" => "Une erreur est survenue lors de l'envoie de votre message. Veuillez réessayer plus tard.",
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR,
-            );
-        }
-    }
-    #[Route('&directory', name: 'add_contact_directory', methods: ['POST'])]
-    public function addDirectory(Request $request, MailerInterface $mailer, ValidatorInterface $validator): JsonResponse
+    #[Route('', name: 'add_contact', methods: ['POST'])]
+    public function add(Request $request, MailerInterface $mailer): JsonResponse
     {
 
     $content = $request->getContent();
-    $data = json_decode($content, true);
+    $data = $request->request->all();
 
-    $constraintViolationList = $validator->validate($data['siteWeb'], [
-        new Assert\Url(),
-    ]);
+    $imagePath = null;
+    $uploadedFile = $request->files->get('image');
 
-    $constraintViolationList = $validator->validate($data['siteWeb'], [
-        new Assert\Regex([
-            'pattern' => '/^(https:\/\/)/',
-            'message' => "Le site web doit commencer par 'https://'",
-        ]),
-    ]);
+    if ($uploadedFile) {
+        $mimeType = $uploadedFile->getMimeType();
+        if (strpos($mimeType, 'image/') === 0) {
+            $imagePath = $uploadedFile->getPathname();
+            $fileName = uniqid() . '.' . $uploadedFile->guessExtension();
+            $uploadedFile->move(
+                $this->getParameter('app.imgDir'),
+                $fileName
+            );
+            $imagePath =  'https://back.unetaupechezvous.fr/upload/img/' . $fileName;
+        } else {
+            // Ce n'est pas une image, renvoyez une erreur de format d'image
+            return $this->json(
+                [
+                    "erreur" => "Le fichier téléchargé n'est pas une image",
+                    "code_error" => Response::HTTP_FORBIDDEN
+                ],
+                Response::HTTP_FORBIDDEN
+            );
+        }
+    }
 
-
-    if (
-        empty($data['name']) ||
-        empty($data['email']) ||
-        empty($data['location']) ||
-        empty($data['postalCode']) ||
-        empty($data['siteWeb']) ||
-        empty($data['service']) ||
-        empty($data['subject']) ||
-        empty($data['directory'])
-        ) {
+    if (strlen($data['postalCode']) !== 5) {
         return $this->json(
             [
-                "erreur" => "Erreur de saisie",
-                "code_error" => 404
+                "erreur" => "Erreur lors de la saisie du code postal (5 chiffres attendus)",
+                "code_error" => Response::HTTP_FORBIDDEN
             ],
-            Response::HTTP_NOT_FOUND, // 404
+            Response::HTTP_FORBIDDEN
         );
     }
 
-    if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+    // if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+    //     return $this->json(
+    //         [
+    //             "erreur" => "Adresse e-mail invalide",
+    //             "code_error" => 400
+    //         ],
+    //         Response::HTTP_BAD_REQUEST, // 400
+    //     );
+    // }
+    try {
+
+        if (isset($data['emailReturn']) && $data['emailReturn'] === 'true') {
+            $emailReturn = (new TemplatedEmail())
+            ->to($data['email'])
+            ->from($_ENV['MAILER_TO'])
+            ->subject('Votre message a bien été envoyé')
+            ->htmlTemplate('emails/contactReturn.html.twig')
+            ->context([
+                'subjectContact' => $data['subject'],
+                'nameContact' => $data['name'],
+                'emailContact' => $data['email'],
+                'phoneContact' => $data['phone'],
+                'postalCodeContact' => $data['postalCode'],
+                'messageContact' => $data['message'] ?? null,
+                'imageContact' =>  $imagePath ?? null,
+                'dateContact' => $data['date'] ?? null,
+                'statusContact' => $data['status'] ?? null,
+                'nameSocietyContact' => $data['nameSociety'] ?? null,
+                'siretContact' => $data['siret'] ?? null,
+                'adressContact' => $data['adress'] ?? null,
+                'surfaceContact' => $data['surface'] ?? null ,
+                'interventionContact' => $data['intervention'] ?? null,
+                'interventionOtherContact' => $data['interventionOther'] ?? null,
+            ]);
+
+            $mailer->send($emailReturn);
+            
+        }
+
+        if ($data['subject'] === 'Webmaster'  ) {
+            $data['subject'] = 'Demande de contact webmaster';
+            $emailTo = $_ENV['MAILER_TO_WEBMASTER'];
+        }
+        else {
+            $emailTo = $_ENV['MAILER_TO'];
+        }
+
+        $email = (new TemplatedEmail())
+            ->to($emailTo)
+            ->from($_ENV['MAILER_TO'])
+            ->subject($data['subject'] . ' de ' . $data['name'])
+            ->htmlTemplate('emails/contact.html.twig')
+            ->context([
+                'subjectContact' => $data['subject'],
+                'nameContact' => $data['name'],
+                'emailContact' => $data['email'],
+                'phoneContact' => $data['phone'],
+                'postalCodeContact' => $data['postalCode'],
+                'messageContact' => $data['message'] ?? null,
+                'imageContact' =>  $imagePath ?? null,
+                'dateContact' => $data['date'] ?? null,
+                'statusContact' => $data['status'] ?? null,
+                'nameSocietyContact' => $data['nameSociety'] ?? null,
+                'siretContact' => $data['siret'] ?? null,
+                'adressContact' => $data['adress'] ?? null,
+                'surfaceContact' => $data['surface'] ?? null ,
+                'interventionContact' => $data['intervention'] ?? null,
+                'interventionOtherContact' => $data['interventionOther'] ?? null,
+                
+            ])
+            ->replyTo($data['email']);
+        
+
+        $mailer->send($email);
+
         return $this->json(
             [
-                "erreur" => "Adresse e-mail invalide",
-                "code_error" => 400
+                "message" => "Votre message a bien été envoyé",
             ],
-            Response::HTTP_BAD_REQUEST, // 400
+            Response::HTTP_OK,
         );
-    }
-    
-    if ($data['subject'] === 'Webmaster') {
-        $data['subject'] = 'Demande de contact webmaster';
-        $emailTo = $_ENV['MAILER_TO_WEBMASTER'];
-    }
-    else {
-        return $this->json(
-            [
-                "erreur" => "Erreur de saisie",
-                "code_error" => 400
-            ],
-            Response::HTTP_BAD_REQUEST, // 400
-        );
-    }
-    $email = (new TemplatedEmail())
-        ->to($emailTo)
+                
+        }
+    catch (\Exception $e) {
+        $email = (new TemplatedEmail())
+        ->to($_ENV['MAILER_TO_WEBMASTER'])
         ->from($_ENV['MAILER_TO'])
-        ->subject($data['subject'] . ' de ' . $data['name'])
-        ->htmlTemplate('emails/contactDirectory.html.twig')
+        ->subject('Erreur lors de l\'envoie de l\'email')
+        ->htmlTemplate('emails/error.html.twig')
         ->context([
-            'emailContact' => $data['email'],
-            'subjectContact' => $data['subject'],
-            'nameContact' => $data['name'],
-            'postalCodeContact' => $data['postalCode'],
-            'locationContact' => $data['location'],
-            'websiteContact' => $data['siteWeb'],
-            'serviceContact' => $data['service'],
-            'directoryContact' => $data['directory'],
-            'directoryOtherContact' => $data['directoryOther'] ?? null,
+            'error' => $e->getMessage(),
+        ]);
+        $mailer->send($email);
 
-        ])
-        ->replyTo($data['email']);
 
-    $mailer->send($email);
-
-    return $this->json(
-        [
-            "message" => "Votre message a bien été envoyé",
-        ],
-        Response::HTTP_OK,
-    );
-    }   
-
+        return $this->json(
+            [
+                "erreur" => "Erreur lors de l'envoie de l'email, veuillez réessayer plus tard",
+                "code_error" => Response::HTTP_FORBIDDEN
+            ],
+            Response::HTTP_FORBIDDEN
+        );
+    }
+    }
 }
