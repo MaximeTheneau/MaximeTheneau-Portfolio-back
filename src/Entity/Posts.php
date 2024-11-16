bce1e1164f5f410748d0f1ffe5377454455da400
Theneau Maxime <theneau.maxime@gmail.com>
2024-11-16 12:52:58 +0100

    Feat: add subcategory api

--- src/Entity/Posts.php
+++ src/Entity/Posts.php
@@ -97,7 +97,7 @@ class Posts
     private ?string $website = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(['api_posts_read', 'api_posts_sitemap', 'api_posts_category', 'api_posts_home'])]
+    #[Groups(['api_posts_read', 'api_posts_sitemap', 'api_posts_category', 'api_posts_home', 'api_posts_subcategory'])]
     private ?string $url = null;
 
     #[ORM\Column(nullable: true)]
