5386ad84f62c7d1371cf9e055e079810c5c5d62c
Theneau Maxime <theneau.maxime@gmail.com>
2024-12-18 14:04:21 +0100

    Feat: Change for stymulus navbar js

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,6 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 import  gptController  from './controllers/gpt_controller.js'
+import navbar_controller from './controllers/navbar_controller.js';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 const app = startStimulusApp(require.context(
@@ -10,3 +11,5 @@ const app = startStimulusApp(require.context(
 
 // register any custom, 3rd party controllers here
 app.register('gpt', gptController);
+app.register('navbar', navbar_controller);
+
---
+++ assets/controllers/navbar_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ["submenu"];
+
+toggle(event) {
+    const submenu = event.target.closest('li').querySelector('[data-target="navbar.submenu"]');
+    
+    if (submenu) {
+      submenu.classList.toggle("hidden");
+    }
+}
+}
